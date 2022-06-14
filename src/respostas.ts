199b65454682ad6494b54a7c5b69556d4f79831a
Leonardo Garcia <leonardogirotogarcia@gmail.com>
2022-06-14 19:00:40 -0300

    Realização do desafio 3

--- src/respostas.ts
+++ src/respostas.ts
@@ -48,3 +48,33 @@ pessoa4 = {
     idade: 19,
     profissao: Profissao.Padeiro
 }
+
+
+
+// Desafio 3
+
+let botaoAtualizar = document.getElementById('atualizar-saldo')!;
+let botaoLimpar = document.getElementById('limpar-saldo')!;
+let soma = document.getElementById('soma')! as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo')!;
+
+let saldoTotal: number = 0
+
+limparSaldo()
+
+function somarAoSaldo(soma: number) {
+        saldoTotal += soma;
+        campoSaldo.innerHTML = saldoTotal.toString();
+}
+
+function limparSaldo() {
+        campoSaldo.innerHTML = '';
+}
+
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(Number(soma.value));
+});
+
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
